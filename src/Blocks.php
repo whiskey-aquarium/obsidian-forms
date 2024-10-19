f6185113ad6e472df325f0da45b33cfc5afa48b3
404-proof <addyoururl@gmail.com>
2024-10-19 11:43:47 -0400

    Conditionally set the parent for the Field block

--- src/Blocks.php
+++ src/Blocks.php
@@ -38,7 +38,27 @@ final class Blocks {
 		$blocks = glob( OBSIDIAN_FORMS_PATH . 'blocks/build/*', GLOB_ONLYDIR );
 
 		foreach ( $blocks as $block ) {
-			register_block_type_from_metadata( $block );
+			$args = [];
+
+			// Conditionally set the parent for the "Field" block.
+			if ( strpos( $block, 'obsidian-form/field-group' ) !== false && ! $this->is_obsidian_form_post_type() ) {
+				$args['parent'] = [ 'obsidian-form/form' ];
+			}
+
+			// Register the block with the modified args.
+			register_block_type_from_metadata( $block, $args );
 		}
 	}
+
+	/**
+	 * Checks if the current post type is obsidian_form.
+	 *
+	 * @return bool
+	 */
+	private function is_obsidian_form_post_type(): bool {
+		$screen = function_exists( 'get_current_screen' ) ? get_current_screen() : null;
+
+		return $screen && 'obsidian_form' === $screen->post_type;
+	}
+
 }
