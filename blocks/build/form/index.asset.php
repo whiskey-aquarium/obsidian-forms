d367e19021ed573c75702c9217ec146fb234c461
404-proof <addyoururl@gmail.com>
2024-10-20 15:31:54 -0400

    Update Button to use variant instead of deprecated isPrimary

--- blocks/build/form/index.asset.php
+++ blocks/build/form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'c0d622d5bbaecb1740e4');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '975665a8a315546d12ab');
